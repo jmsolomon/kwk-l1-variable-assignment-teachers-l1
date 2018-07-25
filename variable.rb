754495db6ee9d418ae5a742f73ec008ecf00cb35
Jason Solomon <jasonmaxsolomon@gmail.com>
2018-07-25 12:46:42 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,4 +1,42 @@
- # Define a local variable called 'greeting',
- # give it the value of the string "Hello World"
-greeting = "Hello World"
-puts greeting 
+# World"
+ 
+# greeting = "Hello World"
+# puts greeting # Define a local variable called 'greeting',
+ # give it the value of the string "Hello 
+ 
+# puts "What is your name"
+# gets 
+ 
+# puts "What is your name"
+# name = gets 
+# puts "Hello #{name}!"
+
+# def about_me
+#   puts "My name is Alex"
+#   puts "I am from Texas"
+#   puts "I am 24 years old"
+#   puts "My favorite food is chocolate"
+# end 
+
+# about_me
+
+# local_greeting = "Shalom"
+# your_name = "Golda"
+# my_name = "Lea"
+# time_of_day = "Life"
+
+# def greeting(local_greeting, your_name, my_name, time_of_day)
+#   puts "#{local_greeting} #{your_name}, I'm #{my_name}, hows your #{time_of_day}?"
+# end
+
+# greeting(local_greeting, your_name, my_name, time_of_day)
+
+
+def greeting(my_name, your_name)
+ puts "Hi #{your_name}, I'm #{my_name}"
+end
+ 
+greeting("Cardi B", "Karlie")
+
+
+
